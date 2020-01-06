75e5303a7a40493f07e6f873e602204e7fe6c5ab
Rose <olaf223224@gmail.com>
2020-01-06 18:16:22 -0500

    remove app.css

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { CssBaseline } from '@material-ui/core';
 
-import './App.css';
+// import './App.css';
 import Frame from './containers/frame';
 
 export default function App () {
