e9353fa98721d6d15cdab437fe14afd420940419
Rose Peck <rosepeck1997@gmail.com>
2020-03-10 11:07:22 -0400

    POST multiple rules

--- routes/api/rule.js
+++ routes/api/rule.js
@@ -5,12 +5,32 @@ const Rule = mongoose.model('Rule');
 
 // POST new rule
 router.post('/', auth.required, (req, res, next) => {
-  const rule = req.body;
-  const finalRule = new Rule(rule);
-  
-  return finalRule.save()
-    .catch((err) => res.json(err))
-    .then(() => res.json({rule: finalRule}));
+  const rules = req.body.rules;
+
+  if (!rules) {
+    return res.status(400).send({
+      errors: 'Expected a top level field called "rules", containing either a single record or a list of records.'
+    })
+  }
+
+  // current preferred function from mongo
+  Rule.insertMany(rules)
+    .catch((err) => { // returns the first error that is encountered. if there is an error, no records are written
+      if(err) {
+        res.json(err);
+        res.status(400); // HTTP 400: Bad Request
+      }
+    })
+    .then((rules) => {
+      if (rules) { // skip this bit if there was an error
+        res.json({
+          saved: rules
+        });
+        res.status(201); // HTTP 201: Created
+      }
+    });
+
+    // look ma! no return statements!
 });
 
 // update rule
@@ -21,7 +41,7 @@ router.post('/:id', auth.required, (req, res) => {
     runValidators: true,
     useFindAndModify: false,
   };
-  
+
   Rule.findByIdAndUpdate(req.params.id, rule, options, (err, rule) => {
     if (err) return res.status(500).send(err);
     return res.status(200).send(rule);
@@ -47,9 +67,9 @@ router.get('/', auth.optional, (req, res, next) => {
 // get all rules matching query
 router.get('/allWhere', auth.optional, (req, res) => {
   const query = req.body;
-  
+
   // some amount of processing will be required, probably.
-  
+
   Rule.find(query, (err, rules) => {
     if (err) return res.status(500).send(err);
     return res.status(200).send(rules);
