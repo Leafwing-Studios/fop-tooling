9c7a52e30a43b9a28171d0f896eddf752f4729de
Rose Peck <rosepeck1997@gmail.com>
2020-03-13 14:28:42 -0400

    pretty up the homepage

--- client/src/containers/home.js
+++ client/src/containers/home.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 
 import {
   Typography,
+  Link
 } from '@material-ui/core';
 
 export default function Home() {
   return (
-    <Typography paragraph>
-      Welcome to the official website for the Fonts of Power roleplaying system. Feel free to click around or whatever I'm not your dad. 
-    </Typography>
+    <>
+      <Typography paragraph>
+        Welcome to the official website for the Fonts of Power roleplaying system.
+      </Typography>
+      <Typography variant='h6'>
+        Links
+      </Typography>
+      <ul>
+        <li>
+          <Link target='_blank' href='https://discord.gg/8kARYKJ'>
+            Official Discord Server
+          </Link>
+        </li>
+        <li>
+          <Link target='_blank' href='https://drive.google.com/drive/u/0/folders/11ergxyGXK1vIjq3nMQCqItZs1h1-W7EF'>
+            Google drive for rulebooks
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 }
