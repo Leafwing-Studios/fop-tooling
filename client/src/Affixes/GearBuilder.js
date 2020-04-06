e24c6ed9dd61c54f7a2f9e034decef90c01a2871
Rose Peck <rosepeck1997@gmail.com>
2020-04-06 10:21:58 -0400

    placeholder gear builder

---
+++ client/src/Affixes/GearBuilder.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+  Typography
+} from '@material-ui/core';
+
+import {
+  ControlledStepper,
+  Title,
+} from '../Common';
+
+export default class GearBuilder extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <p>This is where I'd put my gear builder... IF I HAD ONE</p>
+      </>
+    );
+  }
+}
