8ab78f58603c783cf99efa9b208190f024ee701c
Rose Peck <olaf223224@gmail.com>
2020-06-08 09:17:29 -0500

    basic login and profile display

--- client/src/SmallPages/Login.js
+++ client/src/SmallPages/Login.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   ComingSoon
 } from '../Common';
 import {
-  Button
+  Button,
+  Typography,
 } from '@material-ui/core';
 
-export default function Login() {
-  return (
-    <>
-      <a href="/api/user/google" style={{textDecoration: 'none'}}><Button variant='contained'>Log in with Google</Button></a>
-    </>
-  );
+export default class Login extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      profile: {},
+    };
+  }
+  
+  componentDidMount() {
+    fetch('/api/user/current')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          profile: res,
+        });
+      })
+      .catch(err => console.log(err.response));
+  }
+  
+  render () {
+    return (
+      <>
+        <a href="/api/user/google" style={{textDecoration: 'none'}}><Button variant='contained'>Log in with Google</Button></a>
+        { false && JSON.stringify(this.state.profile) }
+        {
+          this.state.profile.email && (
+            <>
+              <Typography>
+                <b>Email: </b>
+                {this.state.profile.email}
+              </Typography>
+              <Typography>
+                <b>Google ID: </b>
+                {this.state.profile.googleId}
+              </Typography>
+              <Typography>
+                <b>Internal ID: </b>
+                {this.state.profile._id}
+              </Typography>
+            </>
+          )
+        }
+      </>
+    );
+  }
 }
