7884eb4f166667d08cf9a99b58a16cb0f35da28c
Rose Peck <olaf223224@gmail.com>
2020-06-26 09:58:39 -0500

    field validations and comments

--- client/src/Affixes/Edit/AffixEditor.js
+++ client/src/Affixes/Edit/AffixEditor.js
@@ -29,10 +29,11 @@ export default class AffixEditor extends Component {
 	}
 	
 	componentDidMount() {
+		// fetch the appropriate affix from the api. storing this in the redux store is not recommended for two reasons: 1. users can navigate to this page directly and 2. this component should be reusable for other editing or creation contexts
 		fetch(`/api/affix/${this.props.match.params.affixId}`)
 			.then(res => res.json())
 			.then(affix => {
-				const prettyAffix = {...affix, ...{name: titleCase(affix.name)}};
+				const prettyAffix = {...affix, ...{name: titleCase(affix.name)}}; // force title case to make it look nicer. we squash back down to lower case before saving in the submitForm function
 				this.setState({
 					affix: prettyAffix,
 				});
@@ -40,7 +41,26 @@ export default class AffixEditor extends Component {
 			.catch(err => console.log(err.response));
 	}
 	
-	updateAffix(newAffix) {
+	validateCost() { // computes the validations for the cost field. returns any error strings if any, otherwise returns null. this is nice because we can treat the value as either the error string, or as a boolean for whether to enable error mode on the field
+		// must be a number
+		if (isNaN(this.state.affix.cost)) {
+			return "Must be a number";
+		}
+		return null;
+	}
+	
+	validateMaxReplicates() { // validations for the maxReplicates field
+		// must be a number, must be >= 0
+		if (isNaN(this.state.affix.maxReplicates)) {
+			return "Must be a number";
+		}
+		if (parseFloat(this.state.affix.maxReplicates) < 0) {
+			return "Must be greater than or equal to zero";
+		}
+		return null;
+	}
+	
+	updateAffix(newAffix) { // generic affix update callback for form fields
 		const oldAffix=this.state.affix;
 		const affix = {...oldAffix, ...newAffix};
 		
@@ -50,13 +70,14 @@ export default class AffixEditor extends Component {
 	submitForm(ev) {
 		ev.preventDefault();
 		// make the name all lowercase before sending it off
+		// parse the cost and max replicates into floats
 		console.log('this is how we submit!');
 	}
 
 	render() {
 		return (
 			<form onSubmit={this.submitForm} autoComplete='off'>
-				{false && (
+				{false && ( /* debug */
 					<>
 						<p>{JSON.stringify(this.state.affix)}</p>
 						<Divider />
@@ -94,7 +115,7 @@ export default class AffixEditor extends Component {
 							onChange={(ev) => this.updateAffix({descLong: ev.target.value})}
 						/>
 					</Grid>
-					<Grid item sm={2}>
+					<Grid item sm={3}>
 						<FormControl style={{flexGrow: 1}} fullWidth>
 							<InputLabel id='affix-editor-rarity-label'>Rarity</InputLabel>
 							<Select
@@ -132,6 +153,8 @@ export default class AffixEditor extends Component {
 							fullWidth
 							value={this.state.affix.cost || ''}
 							onChange={(ev) => this.updateAffix({cost: ev.target.value})}
+							error={this.validateCost() ? true : false /* this is here to avoid weird proptypes errors from autocasting */}
+							helperText={this.validateCost()}
 						/>
 						<Spacer height={spacerHeight} />
 						<TextField
@@ -141,6 +164,8 @@ export default class AffixEditor extends Component {
 							fullWidth
 							value={this.state.affix.maxReplicates || ''}
 							onChange={(ev) => this.updateAffix({maxReplicates: ev.target.value})}
+							error={this.validateMaxReplicates() ? true : false}
+							helperText={this.validateMaxReplicates() || 'Enter 0 for infinite max replicates'}
 						/>
 					</Grid>
 				</Grid>
