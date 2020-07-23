16e7ebe1ae1580a03bcc7a529f014e1a4181c334
Rose Peck <olaf223224@gmail.com>
2020-07-23 09:58:36 -0500

    "Add X" search select picker // WIP
    needs to have the multi-select thing, and maybe some cleanup

--- client/src/Common/SearchSelect.js
+++ client/src/Common/SearchSelect.js
@@ -18,15 +18,55 @@ const filter = createFilterOptions();
 */
 
 export default function SearchSelect(props) {
+	const [value, setValue] = React.useState(null);
 
-  return (
+  return (		
 		<Autocomplete 
 			id='combo-box'
-			options={props.options}
-			freeSolo={props.freeSolo}
-			selectOnFocus={props.selectOnFocus === null ? true : props.selectOnFocus}
-			fullWidth
+			value={value}
+			onChange={(event, newValue) => {
+				if (typeof newValue === 'string') { // picking a value from the list (should all be strings)
+					setValue(newValue);
+				} else if (newValue && newValue.inputValue) { // adding a new value (this option is an object)
+					setValue(newValue.inputValue);
+				} else { // edge case for if something messes up
+					setValue(newValue);
+				}
+			}}
+			filterOptions={(options, params) => {
+				const filtered = filter(options, params);
+				
+				if (params.inputValue !== '') {
+					filtered.push({
+						inputValue: params.inputValue,
+						title: `Add "${params.inputValue}"`
+					})
+				}
+				
+				return filtered;
+			}}
+			selectOnFocus
 			clearOnBlur
+			handleHomeEndKeys
+			options={props.options}
+			getOptionLabel={(option) => {
+				if (typeof option === 'string') { // all supplied options should be just strings that we can use directly
+					return option;
+				} 
+				
+				if (option.inputValue) { // the "Add X" option is an object, so we need to pull the actual value out of it
+					return option.inputValue;
+				}
+			}}
+			renderOption={(option) => { // as with getOptionLabel, we need to handle the "Add X" case, since it's not just a string
+				if (typeof option === 'string') { 
+					return option;
+				} else { 
+					return option.title;
+				}
+			}}
+			freeSolo
+			fullWidth
 			renderInput={(params) => (
 				<TextField
 					{...params}
